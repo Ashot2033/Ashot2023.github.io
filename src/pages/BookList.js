3abedce897d446d992c6b10d214e995d07b91df0
Markus Wedler <markuswedler8@gmail.com>
2023-01-04 18:05:59 +1000

    .

--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -22,7 +22,7 @@ const BooksList = () => {
       <div className="list">{
         books && books.map(book => (
           <a className="book" href={"/kbooks/#/books/" + book.id} key={book.title}>
-            <img className="cover" src={"https://raw.githubusercontent.com/ashot2003/kbooks/1632e0ba0ce3b7db8daaf8a85bc8c557feafb2ce/public/images/covers/" + book.cover} />
+            <img className="cover" src={"https://raw.githubusercontent.com/ashot2003/kbooks/main/public/images/covers/" + book.cover} />
             <div className="content">
               <div className="type">{ book.type }</div>
               <div className="title">{ book.title }</div>
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -22,7 +22,7 @@ const Details = () => {
       <h1 className="title">{ book && book.title }</h1>
 
       <div className="cont">
-        <img src={book && "https://raw.githubusercontent.com/ashot2003/kbooks/1632e0ba0ce3b7db8daaf8a85bc8c557feafb2ce/public/images/covers/" + book.cover} />
+        <img src={book && "https://raw.githubusercontent.com/ashot2003/kbooks/main/public/images/covers/" + book.cover} />
         <div className="about">
           <ul>
             <li>Автор: <span>Князян А.З.</span></li>
